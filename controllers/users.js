932c35b2eab12f703031204687c801996274f7ef
Rengkat <successinmotion11@gmail.com>
2024-02-06 22:35:53 +0100

    create user fuctionality

--- controllers/users.js
+++ controllers/users.js
@@ -2,27 +2,36 @@ const User = require("../models/users");
 const asyncHandler = required("../midleware/asyncHandler.js");
 const bcrypt = require("bcrypt");
 const bcryptSalt = bcrypt.genSalt(10);
-const createUser = async (req, res) => {
+const createUser = asyncHandler(async (req, res) => {
   try {
     const { firstName, lastName, email, password } = req.body;
-    const UserCreated = await User.create({
+    if (!firstName || !lastName || !email || !password) {
+      throw new Error("Please enter all fields");
+    }
+    const userExist = await User.findOne({ email });
+    if (userExist) {
+      res.status(400).send("User already exist");
+    }
+    await User.create({
       firstName,
       lastName,
       email,
       password: bcrypt.hashSync(password, bcryptSalt),
     });
+    res.status(201).json({ success: true, message: "account created" });
   } catch (error) {
-    console.log(error);
+    throw new Error("Sorry! something went wrong");
+    res.status(404).send("Sorry not found!");
   }
   res.status(201).json({ success: true, message: "User created successfully" });
-};
-const loginUser = (req, res) => {
+});
+const loginUser = asyncHandler(async (req, res) => {
   try {
     res.status(200).send("login");
   } catch (error) {}
-};
-const updateUser = (req, res) => {
+});
+const updateUser = asyncHandler(async (req, res) => {
   try {
   } catch (error) {}
-};
+});
 module.exports = { createUser, loginUser, updateUser };
